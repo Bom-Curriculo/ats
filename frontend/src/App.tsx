7f2d9a709d97ba479fdbb4bb94a890a205e30f4e
Leooozzz <leozeraffcanal2405@gmail.com>
2026-06-30 16:46:57 -0300

    feat: config frontend for use shadcnUI tanstack-query

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <section>
+      <h1 className="text-4xl text-green-400">ATS  <Button>TEST SHADCNUI</Button> </h1>
+    </section>
+  );
+}
+
+export default App;
